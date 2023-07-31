5fd46d3b759ebaf7cdd965700d872c3bc4e9defa
finionb <121615804+finionb@users.noreply.github.com>
2023-07-31 16:54:45 +0200

    Datei hochgeladen

---
+++ gefaehrdeteTierarten_Becker_Bormann_HageHassan/js/opener.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var icons = document.getElementsByClassName('icon');
+    for (var i = 0; i < icons.length; i++) {
+      icons[i].addEventListener('click', function() {
+      });
+    }
+  });
+  
